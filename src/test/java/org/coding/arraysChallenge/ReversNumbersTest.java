9158c0cffe6ce6ffa590ea8bd5698734f722074b
TonyYuta <tonykagaya@gmail.com>
2017-08-27 13:41:51 -0700

    dataProvider for TC was added

--- src/test/java/org/coding/arraysChallenge/ReversNumbersTest.java
+++ src/test/java/org/coding/arraysChallenge/ReversNumbersTest.java
@@ -10,6 +10,7 @@ package org.coding.arraysChallenge;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -33,11 +34,37 @@ public class ReversNumbersTest {
 		rn = new ReversNumbers();
 	}
 	
+	@DataProvider(name = "numbers")
+	public static Object[][] numbers() {
+		return new Object[][] {
+			{12345, 54321},
+			{112, 211},
+			{11345, 54311},
+			{11345, 54311},
+			{11345, 54311},
+			{11345, 54311},
+			{110022, 220011},
+			{11345, 54311},
+			{11345, 54311},
+			{2010101, 1010102},
+			{11345, 54311},
+			{12349, 94321},
+		};
+	}
+	
 	@Test(enabled = true, groups = {"reverse", "numbers", "sanity", "regression", "all"}, priority = 5)
 	public void testReverseNumberUsingStringBuffer() {
 		int origin = 1230506;
 		int expected = 6050321;
 		Assert.assertEquals(rn.reverseNumberUsingStringBuffer(origin), expected, "expecterd reverse number doesn't match to actual");
 	}
+	
+	@Test(enabled = true, timeOut = 1000, dataProvider = "numbers", groups = {"reverse", "numbers", "sanity", "regression", "all"}, priority = 5)
+	public void testReverseNumberUsingStringBufferDataProvider(int origin, int expected) {
+		Assert.assertEquals(rn.reverseNumberUsingStringBuffer(origin), expected);
+	}
+	
+
+	
 
 }
